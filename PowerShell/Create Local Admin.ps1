a7658345e1d041970018f071cbe3bbf2fc398e32
Ash Forde <Ashley.Forde@hud.govt.nz>
2024-03-19 14:22:52 +1300

    Updates

--- PowerShell/Create Local Admin.ps1
+++
@@ -1,8 +0,0 @@
-# New Account
-New-LocalUser -Name "administratoraccounthere" -Password (ConvertTo-SecureString "superstrongpasswordhere" -AsPlainText -Force) -Description "Administrator"
-Add-LocalGroupMember -Group "Administrators" -Member "HUDAdmin"
-
-
-# Update password
-$account = Get-LocalUser -Name "NewAdminAccount"
-$account | Set-LocalUser -Password (ConvertTo-SecureString "NewPassword" -AsPlainText -Force)
